1f580830d2080ac0c4cfa76312a608c1f0c1e5ee
Shubham Pawar <shubhampawar2321@gmail.com>
2025-03-21 13:15:01 +0530

    commit change

--- AUTO_TEST/cypress/e2e/dashboardPage.cy.js
+++ AUTO_TEST/cypress/e2e/dashboardPage.cy.js
@@ -20,14 +20,7 @@ describe('Dashboard page Test cases', () => {
     });
 
 
-    it('Verify that the user’s name is displayed correctly', () => {
-
-        cy.get('.oxd-userdropdown-name').should('exist')
-            .and('be.visible')
-            .and('contain', 'Le Anh Bao');
-    });
-
-    it('Verify that the correct user name is displayed on dashboard', function () {
+        it('Verify that the correct user name is displayed on dashboard', function () {
         cy.fixture('example').then((data) => {
             cy.get('.oxd-userdropdown-name')
                 .should('exist') 
